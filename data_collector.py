2a1462b33a1ca0c957af9da7fca79a3dd14986a6
laura chan <laura.chan.2010@gmail.com>
2020-05-25 12:12:06 -0400

    refactor the code

--- data_collector.py
+++ data_collector.py
@@ -17,10 +17,10 @@ def lambda_handler(event, context):
         stock_data = yf.Ticker(stock).history(start="2020-05-14", end="2020-05-15", interval="1m")
         for index, row in stock_data.iterrows():
             data = {"high": row.High, "low": row.Low, "ts": str(index), "name": stock}
-            as_jsonstr = json.dumps(data)
+            ticket = json.dumps(data)
             fh.put_record(
                 DeliveryStreamName="test-delivery-stream",
-                Record={"Data": as_jsonstr.encode('utf-8')})
+                Record={"Data": ticket.encode('utf-8')})
 
     return {
         'statusCode': 200,
