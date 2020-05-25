806af9c555fe386ec0c75aaded14e5c48df46a33
laura chan <laura.chan.2010@gmail.com>
2020-05-25 03:21:28 -0400

    remove screenshot into resources folder

--- query.sql
+++ query.sql
@@ -1,3 +1,3 @@
-select g.name as ticket, g.HighestHourlyPrice, s.ts as Datetime from
-(select name, max(high) as HighestHourlyPrice from stock group by name) g, stock s
+select g.name as ticket, g.HighestHourlyPrice, s.ts as Datetime, SUBSTRING(s.ts, 12, 2) AS hour from
+(select name, max(high) as HighestHourlyPrice from "stock-db"."18" group by name) g, "stock-db"."18" s
 where g.name=s.name and g.HighestHourlyPrice=s.high
