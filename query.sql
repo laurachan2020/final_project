eacd507083f0738be3920810c74dd965525c3ad1
laura chan <laura.chan.2010@gmail.com>
2020-05-25 13:00:51 -0400

    update analysis query and export output

--- query.sql
+++ query.sql
@@ -1,3 +1,8 @@
-select g.name as ticket, g.HighestHourlyPrice, s.ts as Datetime, SUBSTRING(s.ts, 12, 2) AS hour from
-(select name, max(high) as HighestHourlyPrice from "stock-db"."18" group by name) g, "stock-db"."18" s
-where g.name=s.name and g.HighestHourlyPrice=s.high
+SELECT name as ticket_name, high as hourly_high_price, ts as timestamp, hour
+FROM(
+ SELECT *,SUBSTRING(ts, 12, 2) AS hour, 
+ ROW_NUMBER() OVER(PARTITION BY name, SUBSTRING(ts, 12, 2) ORDER BY high desc) as row_num
+ FROM "stock-db"."18" 
+) d
+WHERE row_num=1 
+ORDER BY name, ts
