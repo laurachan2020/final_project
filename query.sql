18942fac7a8954286bf360b32179fe9a964763fa
laura chan <laura.chan.2010@gmail.com>
2020-05-25 03:02:11 -0400

    update Athena sql query and export output

--- query.sql
+++ query.sql
@@ -1,1 +1,3 @@
-select name, max(high) as HighestHourlyPrice from stock group by name
+select g.name as ticket, g.HighestHourlyPrice, s.ts as Datetime from
+(select name, max(high) as HighestHourlyPrice from stock group by name) g, stock s
+where g.name=s.name and g.HighestHourlyPrice=s.high
